31cc7a3790f1f83ea91370dce43bce716f7bad54
dbuiviet-xcomp <dbuiviet.xcomp@gmail.com>
2024-10-07 20:46:43 +0700

    Added ExpoRNProject to Github

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {setStatusBarBackgroundColor, StatusBar} from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import {useState} from 'react';
 
 export default function App() {
+  const [enteredText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    //console.log(enteredText);
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    //setCourseGoals([...courseGoals, enteredText]);
+    setCourseGoals(currentCourseGoals => [...courseGoals, enteredText]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.appContainer}>
+      {/* <Text style={styles.dummyText}>
+        Hello React Native with Expo bundled!
+      </Text>
+      <View>
+        <Text
+          style={{margin: 16, borderWidth: 2, borderColor: 'red', padding: 16}}>
+          {/*Not recommended way of styling, use StyleSheet*/}
+      {/* Enjoy Learning React Native!
+        </Text>
+      </View>
+      <Button title="Click me" /> */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goals"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal" onPress={addGoalHandler} />
+      </View>
+      <ScrollView style={styles.goalContainer}>
+        {courseGoals.map(goal => (
+          // <Text style={styles.goalItem} key={goal}>
+          //   {goal}
+          // </Text>
+          // To overcome differences between iOS and Android, we nest <Text> component into <View>
+          <View key={goal} style={styles.goalItem}>
+            <Text style={{color: 'white'}}>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // dummyText: {
+  //   margin: 12,
+  //   padding: 12,
+  //   borderWidth: 1,
+  //   borderColor: 'blue',
+  // },
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 16, //left & right padding
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalContainer: {
+    flex: 4,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    //color: 'white',
   },
 });
